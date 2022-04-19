9d151a81edfb049b0bab2e3ce271f0da86f620d3
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2022-04-19 08:47:49 +0700

    add login feature

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.post('/login', (req, res) => {
+  res.status(200).json({ access_token: 'dw0ady9aydh79aw8dg7a8wgdaw' })
+})
