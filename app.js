42644dd5e706425a19671954d716d18305740115
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2022-04-19 09:01:30 +0700

    add fetch product feature

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ const app = express()
 app.post('/login', (req, res) => {
   res.status(200).json({ access_token: 'dw0ady9aydh79aw8dg7a8wgdaw' })
 })
+
+app.get('/products', (req, res) => {
+  let product = [
+    {
+      id: 1,
+      name: 'Shoes'
+    },
+    {
+      id: 2,
+      name: 'Shirt'
+    }
+  ]
+  res.status(200).json(product)
+})
